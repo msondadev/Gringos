2fcb5af7ab1015e4db18e79f81b6bdab4af356a8
Tinc0d3 <martindanielsonda@gmail.com>
2025-11-28 17:42:31 -0300

    fix: cambios en CSS

--- login.php
+++ login.php
@@ -1,22 +1,20 @@
 <?php
-
 session_start();
+include("conexion.php"); // Conexión a la BD
 
-include("conexion.php"); //Conexión a la BD
-
-if (isset($_POST['login'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = trim($_POST['email']);
     $password = trim($_POST['password']);
 
-    //Verificar si el usuario existe
-    $query = "SELECT * FROM usuarios where email = ?";
+    // Verificar si el usuario existe
+    $query = "SELECT * FROM usuarios WHERE email = ?";
     $stmt = $conexion->prepare($query);
-    $stmt->bind-param("s", $email);
+    $stmt->bind_param("s", $email);
     $stmt->execute();
-    $result = stmt->get_result();
+    $result = $stmt->get_result();
     $user = $result->fetch_assoc();
 
-    if (user && password_verify($password, $user['password'])) {
+    if ($user && password_verify($password, $user['password'])) {
         // Si el login es exitoso, iniciamos la sesión
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['user_name'] = $user['nombre'];
@@ -27,3 +25,4 @@ if (isset($_POST['login'])) {
     }
 }
 ?>
+
